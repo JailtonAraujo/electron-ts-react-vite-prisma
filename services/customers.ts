0a8044ae71b7c1b2b93725fa5603e17c87567510
JailtonAraujo <jailtonraneimoney@gmail.com>
2024-08-18 00:53:58 -0300

    ref: add

--- services/customers.ts
+++
@@ -1,7 +0,0 @@
-const { ipcMain } = require('electron');
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-ipcMain.handle('customer:list', async () => {
-    return await prisma.people.findMany();
-});
